057f77262efc3df4afc6b0cc6239184d15ed28bb
nasir1196 <nasiruddin.cf@gmail.com>
2023-06-20 20:52:15 +0600

    error solved

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,6 @@ import Navbar from './Navbar';
 import './globals.css';
 import { Inter } from 'next/font/google';
 
-const inter = Inter( { subsets: [ 'latin' ] } );
 
 export const metadata = {
   title: 'One Call Kuwait',

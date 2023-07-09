d57c3d0d1d4f42d1ee8aa549bfa1dce33599aa59
nasir1196 <nasiruddin.cf@gmail.com>
2023-07-09 02:00:38 -0700

    action done

--- components/Shared/Footer/Footer.js
+++ components/Shared/Footer/Footer.js
@@ -14,110 +14,110 @@ import WhatsAppIcon from '@mui/icons-material/WhatsApp';
 import InstagramIcon from '@mui/icons-material/Instagram';
 
 
-const Footer = () =>
-{
-    const isMobileScreen = useMediaQuery( "(max-width: 940px)", { noSsr: true } );
+const Footer = () => {
+    const isMobileScreen = useMediaQuery("(max-width: 940px)", { noSsr: true });
     const date = new Date();
     const fullYear = date.getFullYear();
 
     return (
-        <Box sx={ { backgroundColor: "#041B34" } }>
-            <Box sx={ { p: { md: "2rem", xs: "1rem" } } }>
-                <Box sx={ { m: { md: "2rem", xs: "1rem" }, borderBottom: "0.1rem dotted gray", p: "2rem" } }>
-                    <Grid container spacing={ 4 } >
-                        <Grid item xs={ 12 } md={ 6 }>
+        <Box sx={{ backgroundColor: "#041B34" }}>
+            <Box sx={{ p: { md: "2rem", xs: "1rem" } }}>
+                <Box sx={{ m: { md: "2rem", xs: "1rem" }, borderBottom: "0.1rem dotted gray", p: "2rem" }}>
+                    <Grid container spacing={4} >
+                        <Grid item xs={12} md={6}>
                             <Link href="/">
-                                <Typography sx={ { color: "#FFFFFF", fontWeight: { md: "bolder", xs: "bold" }, fontSize: { md: "3rem", xs: "2rem" }, "&:hover": { color: "#F7BF23", cursor: "pointer" } } }>One Call Kuwait</Typography>
+                                <Typography sx={{ color: "#FFFFFF", fontWeight: { md: "bolder", xs: "bold" }, fontSize: { md: "3rem", xs: "2rem" }, "&:hover": { color: "#F7BF23", cursor: "pointer" } }}>One Call Kuwait</Typography>
                             </Link>
                         </Grid>
-                        <Grid item xs={ 12 } md={ 6 }>
-                            <Box sx={ { display: { md: "flex", xs: "initial" }, } }>
-                                <Box sx={ { height: "5rem", width: "5rem", borderRadius: "50%", backgroundColor: "#007AFF", m: "0.9rem", display: { md: "block", xs: "none" }, } }>
-                                    <Box sx={ { display: "flex", alignItems: "center", justifyContent: "center", m: "15px auto" } }>
-                                        <CallOutlinedIcon sx={ { color: "#FFFFFF", fontSize: "3.4rem" } } />
-                                    </Box>
+                        <Grid item xs={12} md={6}>
+                            <Box sx={{ display: { md: "flex", xs: "initial" }, }}>
+                                <Box sx={{ height: "5rem", width: "5rem", borderRadius: "50%", backgroundColor: "#007AFF", m: "0.9rem", display: { md: "block", xs: "none" }, }}>
+                                    <a href={`tel:+96566515342`}><Box sx={{ display: "flex", alignItems: "center", justifyContent: "center", m: "15px auto" }}>
+                                        <CallOutlinedIcon sx={{ color: "#FFFFFF", fontSize: "3.4rem" }} />
+                                    </Box></a>
                                 </Box>
-                                <Box sx={ { cursor: "pointer" } }>
-                                    <Typography sx={ { color: "#FFFFFF", fontWeight: { md: "bolder", xs: "bold" }, fontSize: { md: "2rem", xs: "1.5rem" }, "&:hover": { color: "#007AFF" } } }>Book Online or Call</Typography>
-                                    <Typography sx={ { fontWeight: { md: "bolder", xs: "bold" }, color: "#FFFFFF", "&:hover": { color: "#007AFF" }, fontSize: { md: "3.5rem", xs: "2.3rem" } } }>
-                                        <a href={ `tel:+96566515342` }>66515342</a>
+
+                                <Box sx={{ cursor: "pointer" }}>
+                                    <Typography sx={{ color: "#FFFFFF", fontWeight: { md: "bolder", xs: "bold" }, fontSize: { md: "2rem", xs: "1.5rem" }, "&:hover": { color: "#007AFF" } }}><a href={`tel:+96566515342`}>Book Online or Call</a></Typography>
+                                    <Typography sx={{ fontWeight: { md: "bolder", xs: "bold" }, color: "#FFFFFF", "&:hover": { color: "#007AFF" }, fontSize: { md: "3.5rem", xs: "2.3rem" } }}>
+                                        <a href={`tel:+96566515342`}>66515342</a>
                                     </Typography>
                                 </Box>
                             </Box>
                         </Grid>
                     </Grid>
                 </Box>
-                <Box sx={ { borderBottom: "0.1rem dotted gray", } }>
-                    <Grid container spacing={ 4 } sx={ { py: "2rem" } }>
-                        <Grid item xs={ 10 } md={ 3 } sx={ { py: { md: "3rem", xs: "1.5rem" } } }>
-                            <Box sx={ { borderBottom: "0.1rem dotted gray", p: "0.4rem" } }>
-                                <Typography sx={ { fontSize: "1.5rem", fontWeight: "bolder", color: "#F7BF23", "&:hover": { color: "#007AFF" }, cursor: "pointer", textAlign: "center" } }>ABOUT</Typography>
+                <Box sx={{ borderBottom: "0.1rem dotted gray", }}>
+                    <Grid container spacing={4} sx={{ py: "2rem" }}>
+                        <Grid item xs={10} md={3} sx={{ py: { md: "3rem", xs: "1.5rem" } }}>
+                            <Box sx={{ borderBottom: "0.1rem dotted gray", p: "0.4rem" }}>
+                                <Typography sx={{ fontSize: "1.5rem", fontWeight: "bolder", color: "#F7BF23", "&:hover": { color: "#007AFF" }, cursor: "pointer", textAlign: "center" }}>ABOUT</Typography>
                             </Box>
-                            <Box sx={ { color: "#FFFFFF", textAlign: { xs: "center" } } }>
+                            <Box sx={{ color: "#FFFFFF", textAlign: { xs: "center" } }}>
                                 {
-                                    aboutUs.map( ( item, index ) => (
-                                        <ListItemButton key={ index }>
-                                            <ListItemText primary={ <Link href={ item.pathName }>{ item.title }</Link> } />
+                                    aboutUs.map((item, index) => (
+                                        <ListItemButton key={index}>
+                                            <ListItemText primary={<Link href={item.pathName}>{item.title}</Link>} />
                                         </ListItemButton>
-                                    ) )
+                                    ))
                                 }
                             </Box>
                         </Grid>
 
-                        <Grid item xs={ 10 } md={ 3 }>
-                            <Box sx={ { borderBottom: "0.1rem dotted gray", p: "0.4rem" } }>
-                                <Typography sx={ { fontSize: "1.5rem", fontWeight: "bolder", color: "#F7BF23", "&:hover": { color: "#007AFF" }, cursor: "pointer", textAlign: "center" } }>OUR SERVICES</Typography>
+                        <Grid item xs={10} md={3}>
+                            <Box sx={{ borderBottom: "0.1rem dotted gray", p: "0.4rem" }}>
+                                <Typography sx={{ fontSize: "1.5rem", fontWeight: "bolder", color: "#F7BF23", "&:hover": { color: "#007AFF" }, cursor: "pointer", textAlign: "center" }}>OUR SERVICES</Typography>
                             </Box>
-                            <Box sx={ { color: "#FFFFFF" } }>
+                            <Box sx={{ color: "#FFFFFF" }}>
                                 {
-                                    ourService.map( ( item, index ) => (
-                                        <ListItemButton key={ index }>
-                                            <ListItemText primary={ <Link href={ item.pathName }>{ item.title }</Link> } />
+                                    ourService.map((item, index) => (
+                                        <ListItemButton key={index}>
+                                            <ListItemText primary={<Link href={item.pathName}>{item.title}</Link>} />
                                         </ListItemButton>
-                                    ) )
+                                    ))
                                 }
                             </Box>
                         </Grid>
 
-                        <Grid item xs={ 10 } md={ 3 }>
-                            <Box sx={ { borderBottom: "0.1rem dotted gray", p: "0.4rem" } }>
-                                <Typography sx={ { fontSize: "1.5rem", fontWeight: "bolder", color: "#F7BF23", "&:hover": { color: "#007AFF" }, cursor: "pointer", textAlign: "center" } }>QUICK LINKS</Typography>
+                        <Grid item xs={10} md={3}>
+                            <Box sx={{ borderBottom: "0.1rem dotted gray", p: "0.4rem" }}>
+                                <Typography sx={{ fontSize: "1.5rem", fontWeight: "bolder", color: "#F7BF23", "&:hover": { color: "#007AFF" }, cursor: "pointer", textAlign: "center" }}>QUICK LINKS</Typography>
                             </Box>
-                            <Box sx={ { color: "#FFFFFF", textAlign: { xs: "center" } } }>
+                            <Box sx={{ color: "#FFFFFF", textAlign: { xs: "center" } }}>
                                 {
-                                    quickLink.map( ( item, index ) => (
-                                        <ListItemButton key={ index }>
-                                            <ListItemText primary={ <Link href={ item.pathName }>{ item.title }</Link> } />
+                                    quickLink.map((item, index) => (
+                                        <ListItemButton key={index}>
+                                            <ListItemText primary={<Link href={item.pathName}>{item.title}</Link>} />
                                         </ListItemButton>
-                                    ) )
+                                    ))
                                 }
                             </Box>
                         </Grid>
 
-                        <Grid item xs={ 10 } md={ 3 }>
-                            <Box sx={ { borderBottom: "0.1rem dotted gray", p: "0.4rem" } }>
-                                <Typography sx={ { fontSize: "1.5rem", fontWeight: "bolder", color: "#F7BF23", "&:hover": { color: "#007AFF" }, cursor: "pointer", textAlign: "center" } }>OUR REGIONS</Typography>
+                        <Grid item xs={10} md={3}>
+                            <Box sx={{ borderBottom: "0.1rem dotted gray", p: "0.4rem" }}>
+                                <Typography sx={{ fontSize: "1.5rem", fontWeight: "bolder", color: "#F7BF23", "&:hover": { color: "#007AFF" }, cursor: "pointer", textAlign: "center" }}>OUR REGIONS</Typography>
                             </Box>
-                            <Box sx={ { color: "#FFFFFF", textAlign: { xs: "center" } } }>
+                            <Box sx={{ color: "#FFFFFF", textAlign: { xs: "center" } }}>
                                 {
-                                    ourRegion.map( ( item, index ) => (
-                                        <ListItemButton key={ index }>
-                                            <ListItemText primary={ <Link href={ item.pathName }>{ item.title }</Link> } />
+                                    ourRegion.map((item, index) => (
+                                        <ListItemButton key={index}>
+                                            <ListItemText primary={<Link href={item.pathName}>{item.title}</Link>} />
                                         </ListItemButton>
-                                    ) )
+                                    ))
                                 }
                             </Box>
                         </Grid>
                     </Grid>
                 </Box>
-                <Box sx={ { my: { md: "4rem", xs: "1.5rem" }, color: "#FFFFFF", m: "1.2rem" } }>
-                    <Grid container spacing={ 4 }>
-                        <Grid item xs={ 12 } md={ 3 }>
-                            <Box sx={ { display: { md: "flex", xs: "initial" }, justifyContent: { md: "start", xs: "center" }, alignItems: { xs: "center", md: "start" }, cursor: "pointer" } }>
+                <Box sx={{ my: { md: "4rem", xs: "1.5rem" }, color: "#FFFFFF", m: "1.2rem" }}>
+                    <Grid container spacing={4}>
+                        <Grid item xs={12} md={3}>
+                            <Box sx={{ display: { md: "flex", xs: "initial" }, justifyContent: { md: "start", xs: "center" }, alignItems: { xs: "center", md: "start" }, cursor: "pointer" }}>
 
-                                <AddLocationAltIcon sx={ { fontWeight: "bold", fontSize: { xs: '4rem', md: "2rem" }, m: { xs: "0 8.1rem auto", md: "0 0.6rem auto" } } } />
+                                <AddLocationAltIcon sx={{ fontWeight: "bold", fontSize: { xs: '4rem', md: "2rem" }, m: { xs: "0 8.1rem auto", md: "0 0.6rem auto" } }} />
 
-                                <Box sx={ { fontWeight: 'bold', fontSize: "4rem", textAlign: { xs: "center", md: "start" }, "&:hover": { color: "#F7BF23" } } }>
+                                <Box sx={{ fontWeight: 'bold', fontSize: "4rem", textAlign: { xs: "center", md: "start" }, "&:hover": { color: "#F7BF23" } }}>
                                     <Typography>LOCATION</Typography>
                                     <Typography>Kuwait - Kuwait 6 Cites</Typography>
                                 </Box>
@@ -125,25 +125,25 @@ const Footer = () =>
                             </Box>
                         </Grid>
 
-                        <Grid item xs={ 12 } md={ 3 }>
-                            <Box sx={ { display: { md: "flex", xs: "initial" }, justifyContent: { md: "start", xs: "center" }, alignItems: { xs: "center", md: "start" }, cursor: "pointer" } }>
+                        <Grid item xs={12} md={3}>
+                            <Box sx={{ display: { md: "flex", xs: "initial" }, justifyContent: { md: "start", xs: "center" }, alignItems: { xs: "center", md: "start" }, cursor: "pointer" }}>
 
-                                <ForwardToInboxIcon sx={ { fontWeight: "bold", fontSize: { xs: '4rem', md: "2rem" }, m: { xs: "0 8.1rem auto", md: "0 0.6rem auto" } } } />
+                                <ForwardToInboxIcon sx={{ fontWeight: "bold", fontSize: { xs: '4rem', md: "2rem" }, m: { xs: "0 8.1rem auto", md: "0 0.6rem auto" } }} />
 
-                                <Box sx={ { fontWeight: 'bold', fontSize: "4rem", textAlign: { xs: "center", md: "start" }, "&:hover": { color: "#F7BF23", cursor: "pointer" } } }>
+                                <Box sx={{ fontWeight: 'bold', fontSize: "4rem", textAlign: { xs: "center", md: "start" }, "&:hover": { color: "#F7BF23", cursor: "pointer" } }}>
                                     <Typography>Email</Typography>
-                                    <Typography><a href={ `mailto:onecallkuwait1@gmail.com` }>onecallkuwait1@gmail.com</a></Typography>
+                                    <Typography><a href={`mailto:onecallkuwait1@gmail.com`}>onecallkuwait1@gmail.com</a></Typography>
                                 </Box>
 
                             </Box>
                         </Grid>
 
-                        <Grid item xs={ 12 } md={ 3 }>
-                            <Box sx={ { display: { md: "flex", xs: "initial" }, justifyContent: { md: "start", xs: "center" }, alignItems: { xs: "center", md: "start" }, cursor: "pointer" } }>
+                        <Grid item xs={12} md={3}>
+                            <Box sx={{ display: { md: "flex", xs: "initial" }, justifyContent: { md: "start", xs: "center" }, alignItems: { xs: "center", md: "start" }, cursor: "pointer" }}>
 
-                                <QueryBuilderIcon sx={ { fontWeight: "bold", fontSize: { xs: '4rem', md: "2rem" }, m: { xs: "0 8.1rem auto", md: "0 0.6rem auto" } } } />
+                                <QueryBuilderIcon sx={{ fontWeight: "bold", fontSize: { xs: '4rem', md: "2rem" }, m: { xs: "0 8.1rem auto", md: "0 0.6rem auto" } }} />
 
-                                <Box sx={ { fontWeight: 'bold', fontSize: "4rem", textAlign: { xs: "center", md: "start" }, "&:hover": { color: "#F7BF23" } } }>
+                                <Box sx={{ fontWeight: 'bold', fontSize: "4rem", textAlign: { xs: "center", md: "start" }, "&:hover": { color: "#F7BF23" } }}>
                                     <Typography>Open Hour</Typography>
                                     <Typography>Mon-Sun 24/7</Typography>
                                 </Box>
@@ -151,20 +151,20 @@ const Footer = () =>
                             </Box>
                         </Grid>
 
-                        <Grid item xs={ 12 } md={ 3 }>
+                        <Grid item xs={12} md={3}>
                             <Box>
                                 <Link href="https://www.facebook.com">
-                                    <FacebookIcon sx={ { fontSize: { md: "2.4rem", xs: "2rem" }, fontWeight: 'bolder', m: "0.3rem", } } />
+                                    <FacebookIcon sx={{ fontSize: { md: "2.4rem", xs: "2rem" }, fontWeight: 'bolder', m: "0.3rem", }} />
                                 </Link>
                                 <Link href="https://www.twitter.com">
-                                    <TwitterIcon sx={ { fontSize: { md: "2.4rem", xs: "2rem" }, fontWeight: 'bolder', m: "0.3rem", } } />
+                                    <TwitterIcon sx={{ fontSize: { md: "2.4rem", xs: "2rem" }, fontWeight: 'bolder', m: "0.3rem", }} />
                                 </Link>
                                 <Link href="https://www.linkedin.com">
-                                    <LinkedInIcon sx={ { fontSize: { md: "2.4rem", xs: "2rem" }, fontWeight: 'bolder', m: "0.3rem", } } />
+                                    <LinkedInIcon sx={{ fontSize: { md: "2.4rem", xs: "2rem" }, fontWeight: 'bolder', m: "0.3rem", }} />
                                 </Link>
-                                <a href={ `tel:+96566515342` }><WhatsAppIcon sx={ { fontSize: { md: "2.4rem", xs: "2rem" }, fontWeight: 'bolder', m: "0.3rem", } } /></a>
+                                <a href={`tel:+96566515342`}><WhatsAppIcon sx={{ fontSize: { md: "2.4rem", xs: "2rem" }, fontWeight: 'bolder', m: "0.3rem", }} /></a>
                                 <Link href="https://www.instagram.com">
-                                    <InstagramIcon sx={ { fontSize: { md: "2.4rem", xs: "2rem" }, fontWeight: 'bolder', m: "0.3rem", } } />
+                                    <InstagramIcon sx={{ fontSize: { md: "2.4rem", xs: "2rem" }, fontWeight: 'bolder', m: "0.3rem", }} />
                                 </Link>
                             </Box>
                         </Grid>
@@ -172,8 +172,8 @@ const Footer = () =>
                 </Box>
 
             </Box>
-            <Box sx={ { backgroundColor: "#101010", p: { md: "1.5rem", xs: "0.8rem" }, textAlign: "center", color: "#FFFFFF" } }>
-                <Typography>Copyright &copy; { fullYear } E.P.S. | Powered by One-Call-Kuwait</Typography>
+            <Box sx={{ backgroundColor: "#101010", p: { md: "1.5rem", xs: "0.8rem" }, textAlign: "center", color: "#FFFFFF" }}>
+                <Typography>Copyright &copy; {fullYear} E.P.S. | Powered by One-Call-Kuwait</Typography>
             </Box>
         </Box>
     );
--- components/plumbing/ServieArea/ServiceArea.js
+++ components/plumbing/ServieArea/ServiceArea.js
@@ -86,8 +86,8 @@ const ServiceArea = () =>
                     <Typography variant="h3" sx={ { fontWeight: { xs: "bold", md: "bolder" } } }>Get a fast response, call us now</Typography>
 
                     <Box>
-                        <Button variant="contained" sx={ { py: "1rem", px: { xs: "1.5rem", md: "3rem" }, m: "0.8rem", backgroundColor: "#041B34", color: "#FFFFFF", "&:hover": { backgroundColor: "#F7BF23", color: '#041B34' } } }>{ <MoreTimeIcon /> } REQUEST A CALL</Button>
-                        <Button variant="contained" sx={ { py: "1rem", px: { xs: "1.5rem", md: "3rem" }, m: "0.8rem", backgroundColor: "#F7BF23", color: "#041B34", "&:hover": { backgroundColor: "#041B34", color: '#FFFFFF' } } }>{ <TtyIcon /> } +120 012 668</Button>
+                        <Button variant="contained" sx={ { py: "1rem", px: { xs: "1.5rem", md: "3rem" }, m: "0.8rem", backgroundColor: "#041B34", color: "#FFFFFF", "&:hover": { backgroundColor: "#F7BF23", color: '#041B34' } } }>{ <MoreTimeIcon /> } <a href={`tel:+96566515342`}>REQUEST A CALL</a> </Button>
+                        <Button variant="contained" sx={ { py: "1rem", px: { xs: "1.5rem", md: "3rem" }, m: "0.8rem", backgroundColor: "#F7BF23", color: "#041B34", "&:hover": { backgroundColor: "#041B34", color: '#FFFFFF' } } }>{ <TtyIcon /> } <a href={`tel:+96566515342`}>66515342</a></Button>
                     </Box>
                 </Box>
             </Box>
